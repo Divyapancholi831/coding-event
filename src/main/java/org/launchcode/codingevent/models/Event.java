b62c5059308ecb8bacad230baf705291b3ae7e06
Divyapancholi831 <divyapancholi1995@gmail.com>
2021-06-25 00:44:05 -0500

    done with model

---
+++ src/main/java/org/launchcode/codingevent/models/Event.java
@@ -0,0 +1,24 @@
+package org.launchcode.codingevent.models;
+
+public class Event {
+
+    private String name;
+
+
+    public Event(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
