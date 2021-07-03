abfddfe5a08a4eeb7fc9ea6ec014230b8f264397
divya <divyapancholi1995@gmail.com>
2021-07-03 00:34:02 -0500

    done with display validation error in every fields

--- src/main/java/org/launchcode/codingevent/models/Event.java
+++ src/main/java/org/launchcode/codingevent/models/Event.java
@@ -9,43 +9,38 @@ public class Event {
     private int id;
     private static int nextId=1;
 
-    @NotBlank
+    @NotBlank(message="Name is required")
     @Size(min=3,max=50,message = "Name must be between 3 and 50 characters")
     private String name;
 
     @Size(max=500,message = "Description is too long")
     private String description;
 
-    @NotBlank
+    @NotBlank(message="Email is required")
     @Email(message = "invalid email,try again !")
     private String contactEmail;
-
     public Event(String name,String description,String contactEmail) {
         this.name = name;
-        this.description=description;
-        this.contactEmail=contactEmail;
-        this.id=nextId;
-        nextId++;
-    }
-
+       this.description=description;
+       this.contactEmail=contactEmail;
+       this.id=nextId;
+       nextId++;
+   }
     public Event(){}
 
-    public int getId() {
+   public int getId() {
         return id;
     }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
+   public void setName(String name) {
         this.name = name;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
